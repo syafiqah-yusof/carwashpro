b8d0044e40b8a9279a3fadf89e02e678e6316a8a
Sya <nur.syafiqah@firezetta.com>
2026-07-09 16:24:27 +0800

    Hotfix: Set server timezone to Malaysia (Asia/Kuala_Lumpur)

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+// Force the server to use Malaysia Time
+process.env.TZ = 'Asia/Kuala_Lumpur';
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
